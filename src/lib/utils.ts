bb69f689236efc35460a17cc77aadaeb13b37287
AlexKasema <lexxmulwa@gmail.com>
2024-08-20 17:49:09 +0300

    Created and implemented a metadata function interface for easy customization and maintainablility of the application metadata details.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -22,4 +23,36 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice);
+}
+
+export function constructMetadata({
+  title = "FeastDash - where gourmet flavors meet the comfort of your home",
+  description = "FeastDash is a food-ordering web application that aims to simplify and enhance the online food ordering experience.",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      creator: "@LexMulwa",
+    },
+    metadataBase: new URL("http://localhost:3000"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
